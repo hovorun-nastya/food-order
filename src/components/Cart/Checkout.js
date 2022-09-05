0ff54f5c599835c07900596ac3350f838479063c
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-09-05 21:45:40 +0200

    Form validation

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,68 @@
+import classes from './Checkout.module.css';
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
+import React from "react";
+
+const FormSchema = z.object({
+    name: z.string().min(1),
+    street: z.string().min(1),
+    postalCode: z.number().min(5),
+    city: z.string().min(1)
+})
+
+const Checkout = (props) => {
+
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: zodResolver(FormSchema)
+    });
+
+
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+    console.log(errors.name)
+
+    return (
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+            <div className={classes.control}>
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name'
+                       {...register("name")}
+                />
+                {errors.name && <p className={classes.error}>{errors.name.message}</p>}
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='street'>Street</label>
+                <input type='text' id='street'{...register("street")} />
+                {errors.street && <p className={classes.error}>{errors.street.message}</p>}
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='postal'>Postal Code</label>
+                <input type='number' id='postal' {...register("postalCode", {
+                    valueAsNumber: true,
+                })}/>
+                {errors.postalCode && <p className={classes.error}>{errors.postalCode.message}</p>}
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='city'>City</label>
+                <input type='text' id='city'{...register("city")}/>
+                {errors.city && <p className={classes.error}>{errors.city.message}</p>}
+            </div>
+            <pre>{JSON.stringify(watch(), null, 2)}</pre>
+            <div className={classes.actions}>
+                <button type='button' onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button type='submit' className={classes.submit}>Confirm</button>
+            </div>
+        </form>
+    );
+};
+
+export default Checkout;
